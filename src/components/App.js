0803e92b4ba85df81d0f96df10681bd46353bd66
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-05 12:05:28 -0800

    Added Login Component, updated API.js

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Login from './Login';
+import { api } from '../services/api';
+import {Route} from 'react-router-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      auth: {
+        user: {}
+      }
+    };
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      console.log('there is a token');
+      // make a request to the backend and find our user
+      api.auth.getCurrentUser().then(user => {
+        console.log(user)
+        const updatedState = { ...this.state.auth, user: user };
+        this.setState({ auth: updatedState });
+      });
+    }
+  }
+
+  login = data => {
+    const updatedState = { ...this.state.auth, user: data };
+    localStorage.setItem('token', data.jwt);
+    this.setState({ auth: updatedState });  
+  };
+
+  logout = () => {
+    localStorage.removeItem('token');
+    this.setState({ auth: { user: {} } });
+  };
+
+  render() {
+    return (
+      <div className="App">
+            <Route
+              exact
+              path="/login"
+              render={routerProps => <Login {...routerProps} onLogin={this.login} />}
+            />
+      </div>
+    );
+  }
+}
+
+export default App;
