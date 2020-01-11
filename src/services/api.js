1c75d76c36cd0b785a9a2252eb7b68830fcafa7c
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-11 12:27:12 -0800

    added comments to api js file

--- src/services/api.js
+++ src/services/api.js
@@ -1,39 +1,45 @@
-const API_ROOT = `http://localhost:3000/api/v1`;
-const token = localStorage.getItem('token');
+const API_ROOT = `http://localhost:3000/api/v1`; // root url of backend (rails)
+const token = localStorage.getItem("token"); // current value of jwt token in local storage
 
+// headers for all requests never change, so store them in a variable for use for all fetches
 const headers = {
-  'Content-Type': 'application/json',
-  Accepts: 'application/json',
-  Authorization: token
+  "Content-Type": "application/json",
+  Accepts: "application/json",
+  Authorization: token // special note for authorization, if token is not present, requests will have issues
 };
 
+// METHOD: api request for logging a user in and returning the user information (or error)
 const login = data => {
-    return fetch(`${API_ROOT}/login`, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(data)
-    }).then(res => res.json());
-  };
+  return fetch(`${API_ROOT}/login`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(data)
+  }).then(userData => userData.json());
+};
 
-  const signUp = data => {
-    return fetch(`${API_ROOT}/users`, {
-      method: 'POST',
-      headers,
-      body: JSON.stringify(data)
-    }).then(res => res.json());
-  };
-  
-  const getCurrentUser = () => {
-    return fetch(`${API_ROOT}/user_is_authed`, {
-      headers
-    }).then(res => {
-      return res.json()});
-  };
+// METHOD: api request for signing up a NEW user and passing that new user data back (or an error)
+const signUp = data => {
+  return fetch(`${API_ROOT}/users`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(data)
+  }).then(userData => userData.json());
+};
 
-  export const api = {
-    auth: {
-      signUp,
-      login,
-      getCurrentUser
-    }
-  };
+// METHOD: api request for getting a current user (to verify that they are authorized)
+const getCurrentUser = () => {
+  return fetch(`${API_ROOT}/user_is_authed`, {
+    headers
+  }).then(userData => {
+    return userData.json();
+  });
+};
+
+// export all the api service methods for use in other files
+export const api = {
+  auth: {
+    signUp,
+    login,
+    getCurrentUser
+  }
+};
