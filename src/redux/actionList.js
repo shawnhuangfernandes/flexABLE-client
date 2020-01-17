568af9272e1a4f759242f021f4ecdc1cccb8cee6
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-17 09:49:15 -0800

    updated and removed more Calendar Redux state files

--- src/redux/actionList.js
+++ src/redux/actionList.js
@@ -1,6 +1,5 @@
 export { signIn } from "./auth/authActions";
 export { logout } from "./auth/authActions";
-export { setCurrentDate } from './calendar/calendarActions'
 export { getWeekWorkouts } from './workout/workoutActions'
 export { setExerciseList } from './exercise/exerciseActions'
 
--- src/redux/reducerList.js
+++ src/redux/reducerList.js
@@ -1,15 +1,14 @@
 import { authReducer } from "./auth/authReducer";
 import { calendarReducer } from "./calendar/calendarReducer";
 import { combineReducers } from "redux";
-import { workoutReducer } from "./workout/workoutReducer"
-import { exerciseReducer } from "./exercise/exerciseReducer"
+import { workoutReducer } from "./workout/workoutReducer";
+import { exerciseReducer } from "./exercise/exerciseReducer";
 
 // root reducer that contains/combines all the different reducers
 const rootReducer = combineReducers({
   authReducer, // handles authorization state
-  calendarReducer, // handles calendar state
   workoutReducer, // handles workout (planner) state
-  exerciseReducer
+  exerciseReducer // handles exercise state
 });
 
 export default rootReducer;
