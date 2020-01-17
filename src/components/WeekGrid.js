4b5348a96bcc5187347bfd4335a7784581fe210c
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-17 11:21:59 -0800

    createDayCards method for loop working to get all the dates from the beginning of the calendar week

--- src/components/WeekGrid.js
+++ src/components/WeekGrid.js
@@ -5,6 +5,16 @@ const WeekGrid = props => {
   console.log(props.firstDayOfWeek);
   console.log(props.weekWorkouts);
 
+  const weekNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
   // calendar date formatter (for incoming api dates) goes here
   const dateRbFormatter = rbDate => {
     const arr = rbDate.split("-"); // split the string date (e.g "2020-04-06") by dashes
@@ -14,7 +24,17 @@ const WeekGrid = props => {
       Day: parseInt(arr[2])
     };
   };
-  return <div></div>;
+
+  // method that creates Day Cards from week Workouts
+  const createDayCards = () => {
+    for (let i = 0; i < 7; i++) {
+      console.log(props.firstDayOfWeek)
+      props.firstDayOfWeek.setDate(props.firstDayOfWeek.getDate() + 1);
+    }
+
+    return null;
+  };
+  return <div>{createDayCards()}</div>;
 };
 
 export default WeekGrid;
