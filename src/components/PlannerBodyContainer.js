a104291679bcac26d8714a4c911c3f394e1e6525
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-16 18:42:24 -0800

    Added Local State to Planner Component to handle on Click events coming from the calendar. Also create a formatter to take in a Javascript Date and provide a universal date object that will be used for api fetch requests

--- src/components/PlannerBodyContainer.js
+++ src/components/PlannerBodyContainer.js
@@ -1,23 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { api } from '../services/api'
 import { useSelector } from 'react-redux'
+import { setCurrentDate } from '../redux/actionList';
 const PlannerBodyContainer = () => {
 
-  let workoutWeekSelected = useSelector(state => state.workoutReducer.selectedWeekWorkouts) // set up our redux state for the selected week's workouts
-  let exerciseList = useSelector(state => state.exerciseReducer.exercises) // set up our redux state for the available exercises
+  // redux state
+  let workoutWeekSelected = useSelector(state => state.workoutReducer.selectedWeekWorkouts) // selected week's workouts
+  let exerciseList = useSelector(state => state.exerciseReducer.exercises) // available exercises
+
+  // calendar date formatter (Ruby to JS) goes here
+  const dateJsFormatter = jsDate => {
+    return {
+      day: jsDate.getDate(),
+      month: jsDate.getMonth() + 1,
+      year: jsDate.getFullYear()
+    }
+  }
+
+  // local state
+  const [currentDate, setCurrentDate] = useState(dateJsFormatter(new Date()))
 
   useEffect(() => {
-    console.log(exerciseList);
+    console.log(currentDate)
     // let's get our exercises and store them in state
-    // the date to today (in JS)
     // lets get our workouts for this calendarweek an also store them in state
   });
 
-  // calendar date formatter (Ruby to JS) goes here
-
+  
 
   // calendar date formatter (JS to Ruby) goes here
+  const dateRbFormatter = RbDate => {
 
+  }
   
   // mapping method that generates our DayCards goes here
 
