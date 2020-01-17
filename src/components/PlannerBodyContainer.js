cf23b727f5a8fa3103e2b0c86d437604a18e9bbb
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-17 08:44:20 -0800

    Updated code to include a date formatter to take a date from api and convert it into a simple object for use to compare to currentDate

--- src/components/PlannerBodyContainer.js
+++ src/components/PlannerBodyContainer.js
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { api } from '../services/api'
-import { useSelector } from 'react-redux'
-import { setCurrentDate } from '../redux/actionList';
-import Calendar from 'react-calendar'
+import React, { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getWeekWorkouts } from "../redux/actionList";
+import Calendar from "react-calendar";
 
 // Planner Container taht holds Day Cards, and Calendar
 const PlannerBodyContainer = props => {
-
   // redux state
-  let workoutWeekSelected = useSelector(state => state.workoutReducer.selectedWeekWorkouts) // selected week's workouts
+  let workoutWeekSelected = useSelector(
+    state => state.workoutReducer.selectedWeekWorkouts
+  );
+
+  // dispatch
+  const dispatch = useDispatch();
 
   // calendar date formatter (for incoming JS Dates) goes here
   const dateJsFormatter = jsDate => {
@@ -16,46 +20,49 @@ const PlannerBodyContainer = props => {
       day: jsDate.getDate(),
       month: jsDate.getMonth() + 1,
       year: jsDate.getFullYear()
+    };
+  };
+
+  // calendar date formatter (for incoming api dates) goes here
+  const dateRbFormatter = rbDate => {
+    const arr = rbDate.split('-') // split the string date (e.g "2020-04-06") by dashes
+    return {
+      year: parseInt(arr[0]),
+      month: parseInt(arr[1]),
+      Day: parseInt(arr[2])
     }
   }
 
   // local state
-  const [currentDate, setCurrentDate] = useState(dateJsFormatter(new Date()))
+  const [currentDate, setCurrentDate] = useState(dateJsFormatter(new Date()));
 
-  
-  // this method 
+  // this method handles what happens when state changes (or when a re-render occurs)
   useEffect(() => {
-  console.log(currentDate);
     // api service to get the current users workouts grouped by day
-  api.workouts.getCurrentWeekWorkouts(
-    {
-      user_id: props.user.id,
-      date: currentDate
-    }
-  ).then(workoutsForWeek => console.log(workoutsForWeek))
+    api.workouts
+      .getCurrentWeekWorkouts({
+        user_id: props.user.id,
+        date: currentDate
+      })
+      .then(workoutsForWeek => dispatch(getWeekWorkouts(workoutsForWeek))); // dispatch to change the days of the week selected
   }); // ---- IMPORTANT NOTE, the brackets here prevent useEffect from running multiple times
 
-  
-
-  // calendar date formatter (JS to Ruby) goes here
-  const dateRbFormatter = RbDate => {
-
-  }
-  
   // mapping method that generates our DayCards goes here
 
-
   // event handler for calendar click
-  const handleCalendarClick = (date) => {
-    setCurrentDate(dateJsFormatter(date));
-  }
+  const handleCalendarClick = dateSelected => {
+    setCurrentDate(dateJsFormatter(dateSelected)); // set the current date
+  };
 
   return (
     <div>
       <Calendar
-          onChange={handleCalendarClick}
-          value={new Date(currentDate.year, currentDate.month - 1, currentDate.day)}
-        />
+        onChange={handleCalendarClick}
+        value={
+          new Date(currentDate.year, currentDate.month - 1, currentDate.day)
+        }
+        calendarType="US"
+      />
     </div>
   );
 };
