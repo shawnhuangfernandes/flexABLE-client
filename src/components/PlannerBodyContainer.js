e65d8917f2d94edc99ccb4ded39f4908f1109c62
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-17 09:31:06 -0800

    Trouble shooted rootReducer, figured out that my components were being re-rendered when any one piece of state was updated

--- src/components/PlannerBodyContainer.js
+++ src/components/PlannerBodyContainer.js
@@ -7,7 +7,7 @@ import Calendar from "react-calendar";
 // Planner Container taht holds Day Cards, and Calendar
 const PlannerBodyContainer = props => {
   // redux state
-  let workoutWeekSelected = useSelector(
+  let selectedWeekWorkouts = useSelector(
     state => state.workoutReducer.selectedWeekWorkouts
   );
 
@@ -23,18 +23,18 @@ const PlannerBodyContainer = props => {
     };
   };
 
+   // local state
+   const [currentDate, setCurrentDate] = useState(dateJsFormatter(new Date()));
+
   // calendar date formatter (for incoming api dates) goes here
   const dateRbFormatter = rbDate => {
-    const arr = rbDate.split('-') // split the string date (e.g "2020-04-06") by dashes
+    const arr = rbDate.split("-"); // split the string date (e.g "2020-04-06") by dashes
     return {
       year: parseInt(arr[0]),
       month: parseInt(arr[1]),
       Day: parseInt(arr[2])
-    }
-  }
-
-  // local state
-  const [currentDate, setCurrentDate] = useState(dateJsFormatter(new Date()));
+    };
+  };
 
   // this method handles what happens when state changes (or when a re-render occurs)
   useEffect(() => {
@@ -45,9 +45,9 @@ const PlannerBodyContainer = props => {
         date: currentDate
       })
       .then(workoutsForWeek => {
-        console.log(workoutsForWeek)
-        dispatch(getWeekWorkouts(workoutsForWeek))}); // dispatch to change the days of the week selected
-  }, [currentDate]); // ---- IMPORTANT NOTE, the brackets here prevent useEffect from running multiple times
+        dispatch(getWeekWorkouts(workoutsForWeek));
+      }); // dispatch to change the days of the week selected
+  }); // ---- IMPORTANT NOTE, the brackets here prevent useEffect from running multiple times
 
   // mapping method that generates our DayCards goes here
 
--- src/redux/workout/workoutReducer.js
+++ src/redux/workout/workoutReducer.js
@@ -8,8 +8,8 @@ const initialState = {
 // calendar reducer that handles redux state when calendar dates are chosen
 export const workoutReducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_WORKOUTS: // if a date has been chosen 
-    return {
+    case GET_WORKOUTS: // if a date has been chosen
+      return {
         selectedWeeksWorkouts: action.payload // set the selected date state to the payload contained in the action
       };
     default:
