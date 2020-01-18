99a6f8c5f2b38f7700c8d569d17c6e930d5318d3
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-18 14:20:52 -0800

    updated Workout item to get a copy of state, and and update completion and return the new version of the state to be passed to dispatch

--- src/components/WorkoutItem.js
+++ src/components/WorkoutItem.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { api } from '../services/api'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 // This is the workout item that displays a specific workout with a delete button, description, and complete button
 const WorkoutItem = props => {
 
     const dispatch = useDispatch(); // allow this component to change redux state
+    const week = useSelector(state => state.workoutReducer.selectedWeekWorkouts) // get redux state for the selected week
+
+    // this method returns the new version of the week after a specific workout has been updated
+    const updateWorkoutList = updatedWorkout => {
+
+        // map through the week, and find the day which the updated workout resides, and update the entry
+        const updatedWeek = week.map(day => {
+            if(day[0].workout_date === updatedWorkout.workout_date){
+                return day.map(workout => {
+                    return workout.id !== updatedWorkout.id ? workout : updatedWorkout
+                })
+            }
+            else
+            {
+                return day
+            }  
+        })
+
+        return updatedWeek
+    }
 
     const toggleCompletion = (e) => {
 
@@ -14,9 +34,9 @@ const WorkoutItem = props => {
             ...props.workout,
             completed: !props.workout.completed
         })
-        .then(updatedWorkout => {
-            console.log(updatedWorkout);
-        })
+        .then(updatedWorkout => 
+            updateWorkoutList(updatedWorkout) // dispatch the UPDATED WORKOUT LIST
+        )
     }
 
     return (
