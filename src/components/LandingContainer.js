207a222d769888b0c79d4f4df4161be570333065
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-01-21 15:56:06 -0800

    Embedded a sample video on landing page and added an on click event to make it play

--- src/components/LandingContainer.js
+++ src/components/LandingContainer.js
@@ -1,53 +1,70 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
+// React specific imports
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import ReactPlayer from 'react-player'
+
+// MUI imports
 import { makeStyles } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
 import NavigationIcon from "@material-ui/icons/Navigation";
-import { Link } from "react-router-dom";
+import  Box from "@material-ui/core/Box"
+
 
 // Set up the styles used in the Landing Container
 const useStyles = makeStyles(theme => ({
   root: {
-    minHeight: "100vh"
+    minHeight: "100vh",
+
   },
-  container: {
-    height: '100%'
+  playButton: {
+    paddingTop: '10px',
+    paddingBottom: '60px'
+  },
+  containerTop: {
+    paddingBottom: '40px'
   },
   extendedIcon: {
     marginRight: theme.spacing(1)
-  }
+  },
 }));
 
 // Renders the Landing Container
 const LandingContainer = () => {
   const classes = useStyles(); // rename the styles created above
 
+  const [playStatus, setPlayStatus] = useState(false)
+
+  const playVideo = () => {
+    setPlayStatus(true);
+  }
+
   return (
-    <div className={classes.root}>
-      <Grid
-        container
-        direction="column"
-        alignItems="center"
-      >
-        <Grid item sm={9}></Grid>{" "}
-        {/* Divides the 9/12ths of the page to reside on top */}
-        <Grid item sm={3}>
-          {" "}
-          {/* Divides the 3/12ths of the page to reside on bottom, and puts content in this part */}
-          <Link to="/login">
-            <Fab
-              variant="extended"
-              color="primary"
-              aria-label="add"
-              className={classes.margin}
-            >
-              <NavigationIcon className={classes.extendedIcon} />
-              Extended
-            </Fab>
-          </Link>
-        </Grid>
-      </Grid>
-    </div>
+    <Box className={classes.root} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <Box className={classes.containerTop} display="flex" flexDirection="column" alignItems="center">
+      <ReactPlayer className={classes.video} url='http://techslides.com/demos/sample-videos/small.mp4' playing={playStatus} controls={false} height={680} width={1220}/>
+      <Fab className={classes.playButton}
+            onClick={playVideo}
+            variant="extended"
+            color="secondary"
+            className={classes.margin}
+          >
+            <NavigationIcon className={classes.extendedIcon} />
+            Play Video
+          </Fab>
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Link to="/login">
+          <Fab
+            variant="extended"
+            color="primary"
+            className={classes.margin}
+          >
+            <NavigationIcon className={classes.extendedIcon} />
+            Enter
+          </Fab>
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
